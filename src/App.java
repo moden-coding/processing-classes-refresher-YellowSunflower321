64e283cc3899ba40ce97e77482a917a0a1caf7eb
Shaili <27ssegre@gannacademy.org>
2026-01-05 11:37:35 -0500

    done

--- src/App.java
+++ src/App.java
@@ -3,6 +3,10 @@ import java.util.*;
 
 public class App extends PApplet{
     ArrayList<Enemy> enemies = new ArrayList<>();
+    float positionX;
+    float positionY;
+    float size = 100;
+    ArrayList<Enemy> list = new ArrayList<>();
 
     public static void main(String[] args)  {
         PApplet.main("App");
@@ -17,13 +21,26 @@ public class App extends PApplet{
     }
 
     public void draw(){
-
-
+        for(Enemy image : list){
+            displayEnemy();
+        }
     }
 
     public void mousePressed(){
-        
+        if(mousePressed){
+            Enemy enemy = new Enemy(this, mouseX, mouseY);
+            positionX = mouseX;
+            positionY = mouseY;
+            list.add(enemy);
+        }
     }
+
+    public void displayEnemy(){
+        fill(200,100,100);
+        circle(positionX,positionY,size);
+
+    }
+
 
 
 }
